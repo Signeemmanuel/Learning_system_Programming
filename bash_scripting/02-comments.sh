637b226b377e8bf05b8e1ab20854e45b8097f46b
Signeemmanuel <signeemmanuel28@gmail.com>
2023-04-25 19:13:58 +0100

    Add $file

---
+++ bash_scripting/02-comments.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+echo "COMMENTS"
+
+# This is a single line comment.
+
+: '
+This
+is
+a
+multi-line
+comment'
+
+cat << put_any_word
+This is a multi-line comment
+This comments type of comment is displayed on the screen
+put_any_word
