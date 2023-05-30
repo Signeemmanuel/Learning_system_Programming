3b89410b8ced9fb1b4456f333922d465959452f6
Signeemmanuel <signeemmanuel28@gmail.com>
2023-05-30 16:36:52 +0100

    Add 1-catch_signals.c

---
+++ Tutorials_on_Signals/1-catch_signals.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+static void signal_handler(int signo)
+{
+    if(signo == SIGINT)
+        printf("Cought SIGINT\n");
+    else if(signo == SIGTERM)
+        printf("Cought SIGTERM\n");
+    exit(EXIT_SUCCESS);
+}
+
+int main()
+{
+    if(signal(SIGINT, signal_handler) == SIG_ERR)
+    {
+        fprintf(stderr, "Cannot handle SIGINT\n");
+        exit(-1);
+    }
+        
+    if(signal(SIGTERM, signal_handler) == SIG_ERR)
+    {
+        fprintf(stderr, "Cannot handle SIGTERM\n");
+        exit(-1);
+    }  
+    while(1);
+}
