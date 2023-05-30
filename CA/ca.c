37acd6ca5193c73aa98bcfdfaedd2cc6ff14c665
Signeemmanuel <signeemmanuel28@gmail.com>
2023-05-30 16:36:51 +0100

    Add ca.c

---
+++ CA/ca.c
@@ -0,0 +1,8 @@
+/*
+1) write a program to illustrate user definded Signal between parent and child processes. ie generating a signal
+from  parent and implement the habdler from the child process
+
+
+2) Write a shell script to open some file and perform a word count
+
+*/
