12599ff543ff6f0776dc6110499553e0b525c0e2
Signeemmanuel <signeemmanuel28@gmail.com>
2023-05-30 16:36:52 +0100

    Add exercise_2.c

---
+++ Tutorials_on_processes_and_signals/exercise_2.c
@@ -0,0 +1,3 @@
+/*
+Write a program that creates a shared memory segment using the shmget() system call. Two child processes should be created, and they should communicate with each other through the shared memory segment. Use signals to synchronize access to the shared memory between the child processes.
+*/
