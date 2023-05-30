166a1e75b7885d9c52946dbef7946e27c4ca0a23
Signeemmanuel <signeemmanuel28@gmail.com>
2023-05-30 16:36:52 +0100

    Add 1-get_Id_of_child_and_perent.c

---
+++ Tutorials_on_Processes/1-get_Id_of_child_and_perent.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    pid_t id;
+
+    printf("Perent Process: Executed by the perent process before fork() - PID = (%d)\n", getpid());
+
+    id = fork();
+
+    if(id < 0)
+    {
+        printf("\n fork failed");
+        exit(-1);
+    }
+
+    if(id > 0)
+    {
+        
+        printf("\n Perent Process: I have created process withID = (%d)\n", id);
+    }
+    else
+    {
+        printf("\n I am child process, id value is (%d) \n", id);
+        printf("\n Child process id is (%d) \n", getpid());
+        printf("\n The creator of the child process is (%d) \n", getppid());
+    }
+    return 0;
+
+}
